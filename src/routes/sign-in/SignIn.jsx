996ff7c837279b13bc7db578b54c819bd396bab6
Elian Avalos <elianemanuelstudy@gmail.com>
2022-06-18 03:39:07 -0400

    Installed firebase Used auth to get user object

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { signInWithGooglePopup } from "../../utils/firebase";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const handleSignIn = async () => {
+    try {
+      const { user } = await signInWithGooglePopup();
+      console.log(user);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return <button onClick={handleSignIn}>Sign in with google</button>;
 };
 
 export default SignIn;
---
+++ src/utils/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzmBVBlr4hiBLjR8OSxz0hX5fnhuJ7oZM",
+  authDomain: "crown-5c5b2.firebaseapp.com",
+  projectId: "crown-5c5b2",
+  storageBucket: "crown-5c5b2.appspot.com",
+  messagingSenderId: "1093731440816",
+  appId: "1:1093731440816:web:7fd570212a24337a8a8375",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
