a4e2d3012a46b76e413f4ab2a83f46e0343c6952
Elian Avalos <elianemanuelstudy@gmail.com>
2022-06-19 18:10:20 -0400

    Fixed button bug

--- src/components/sign-in-form/SignInForm.jsx
+++ src/components/sign-in-form/SignInForm.jsx
@@ -57,7 +57,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign in</Button>
-          <Button buttonType="google" onClick={handleSignIn}>
+          <Button type="button" buttonType="google" onClick={handleSignIn}>
             Sign in with google
           </Button>
         </div>
