c2bdf48a0e99567f207884a2dc16f2713ef0f559
Elian Avalos <elianemanuelstudy@gmail.com>
2022-06-20 03:50:58 -0400

    Structured and Styled Shop page and product-card

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useContext } from "react";
+import { ProductsContext } from "../../context/Products";
+
+import ProductCard from "../../components/product-card/ProductCard";
+
+import "./Shop.scss";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  const { products } = useContext(ProductsContext);
+  return (
+    <div className="products-container">
+      {products &&
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+    </div>
+  );
 };
 
 export default Shop;
