517b72ec3a549e3077f67112d9b52b732ddf5a16
Elian Avalos <elianemanuelstudy@gmail.com>
2022-06-29 14:16:37 -0400

    Added navigation

--- src/components/category-item/CategoryItem.jsx
+++ src/components/category-item/CategoryItem.jsx
@@ -1,17 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import {
   BackgroundImage,
   Body,
   CategoryContainer,
 } from "./CategoryItem.styles.js";
 
-const CategoryItem = ({ title, imageUrl }) => (
-  <CategoryContainer>
-    <BackgroundImage imageUrl={imageUrl} />
-    <Body>
-      <h2>{title}</h2>
-      <p>Shop Now</p>
-    </Body>
-  </CategoryContainer>
-);
+const CategoryItem = ({ title, imageUrl }) => {
+  const navigate = useNavigate();
+  const redirectToCategory = () => navigate(`shop/${title}`);
+  return (
+    <CategoryContainer onClick={redirectToCategory}>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </Body>
+    </CategoryContainer>
+  );
+};
 
 export default CategoryItem;
