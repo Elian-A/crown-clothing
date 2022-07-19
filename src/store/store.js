146f188b75fe332155efd2861f5286c254f3390a
Elian Avalos <elianemanuelstudy@gmail.com>
2022-07-19 15:31:37 -0400

    Adding a custom logger

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,26 @@ import { createStore, compose, applyMiddleware } from "redux";
 import { logger } from "redux-logger";
 import { rootReducer } from "./rootReducer";
 
-const middlewares = [logger];
+const modestLogger = (store) => (next) => (action) => {
+  const { type, payload } = action;
+  if (!type) {
+    return next(action);
+  }
+  console.log("store state:", store.getState());
+  console.log("type:", type);
+  console.log("payload:", payload);
+
+  next(action);
+
+  console.log("current state:", store.getState());
+};
+
+const middlewares = [modestLogger];
 const enhancer = compose(applyMiddleware(...middlewares));
 
 export const store = createStore(
   rootReducer,
   undefined,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  enhancer
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
